2e3187d6cc8f624992b2afdad08ee4fffe701d1f
Fandry07 <fandresena2902@gmail.com>
2025-06-07 16:33:39 +0300

    add daily product

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import LoginForm from '../components/Login/LoginForm';
+import DailyProduct from "../components/Login/DailyProduct";
+import LoginForm from "../components/Login/LoginForm";
 
 function LoginPage() {
   return (
-    <div className='bg-cover bg-no-repeat bg-center h-screen w-screen flex justify-center items-center' style={{backgroundImage: 'url("/img/login-page/bgLoginPage.png")'}}>
-        <LoginForm />
+    <div
+      className="bg-cover bg-no-repeat bg-center h-screen w-screen flex justify-around items-center "
+      style={{ backgroundImage: 'url("/img/login-page/bgLoginPage.png")' }}
+    >
+      <LoginForm />
+      <DailyProduct />
     </div>
-  )
+  );
 }
 
 export default LoginPage;
