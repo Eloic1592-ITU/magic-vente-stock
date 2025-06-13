99788eba60013a033d12388f572a63a31cec91c7
Jonathan <jonathanandrinantenaina@gmail.com>
2025-06-13 21:37:12 +0300

    -

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div className='h-screen w-auto'
+    style={{backgroundImage: "url(/img/home-page/)"}}
+    >
+
+    </div>
+  )
+}
+
+export default HomePage;
