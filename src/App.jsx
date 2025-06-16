4c33d4c4e7076ab64635253f06d6df817e44a492
Moucchi <oksamayami@gmail.com>
2025-06-16 19:02:41 +0300

    feat/auth: modifie App pour appliquer les differentes types de routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {AuthProvider} from './contexts/AuthContext';
+import PrivateRoute from "./routes/PrivateRoute.jsx";
 
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <Router>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/login" element={<LoginPage/>}/>
+
+                    <Route element={<PrivateRoute/>}>
+                        <Route path="/" element={<HomePage/>}/>
+                    </Route>
+                </Routes>
+            </AuthProvider>
+        </Router>
+    );
 }
 
 export default App;
