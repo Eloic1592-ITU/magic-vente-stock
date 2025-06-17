89e516aac9ab89aac6762dd5c656941150c0b2cc
Fandry07 <fandresena2902@gmail.com>
2025-06-17 10:14:02 +0300

    Rename file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {AuthProvider} from './contexts/AuthContext';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 import PrivateRoute from "./routes/PrivateRoute.jsx";
 
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
+import PageAccueil from "./pages/PageAccueil";
+import PageAccueilPerso from "./pages/PageAccueilPerso.jsx";
 
 function App() {
-    return (
-        <Router>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/login" element={<LoginPage/>}/>
+  return (
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<PageAccueil />} />
 
-                    <Route element={<PrivateRoute/>}>
-                        <Route path="/" element={<HomePage/>}/>
-                    </Route>
-                </Routes>
-            </AuthProvider>
-        </Router>
-    );
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<PageAccueilPerso />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/PageAccueil.jsx
+++ src/pages/PageAccueil.jsx
@@ -1,11 +1,11 @@
 import DailyProduct from "../components/Login/DailyProduct";
 import LoginForm from "../components/Login/LoginForm";
 
-function LoginPage() {
+function PageAccueil() {
   return (
     <div
       className="bg-cover bg-no-repeat bg-center h-screen w-screen grid grid-cols-12 items-center px-8"
-      style={{ backgroundImage: 'url("/img/login-page/bgLoginPage.png")' }}
+      style={{ backgroundImage: 'url("/img/login-page/bgPage.png")' }}
     >
       {/* <LoginForm />
       <DailyProduct /> */}
@@ -22,4 +22,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default PageAccueil;
